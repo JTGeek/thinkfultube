0e57de810defb131e91b3a0197add69f39c91922
James Timberlake <James.Timberlake.1018@gmail.com>
2017-04-18 13:48:00 -0400

    2nd commit

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
-searchFunction() {
-  $("form").on("submit", function (e) {
-    e.preventDefault();
-    // prepare the request
-    function handleAPILoaded();
-  });
-}
+function init() {
+  $("form").on("submit", function () {
+    preventDefault();
+    console.log('passed submit button');
 
+    var q = $('#searchBox').val();
+    console.log(q);
+    var request = gapi.client.youtube.search.list({
+      q: q,
+      part: 'snippet',
+      type: 'video',
+    });
 
-function handleAPILoaded() {
-  $('#search-button').attr('disabled', false);
-}
-
-// Search for a specified string.
-function search() {
-  var q = $('#query').val();
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    part: 'snippet',
-    type: 'video',
-  });
-
-  request.execute(function (response) {
-    var str = JSON.stringify(response.result);
-    $('#results').html('<pre>' + str + '</pre>');
-  });
-}
+    request.execute(function (response) {
+      var str = JSON.stringify(response.result);
+      $('#results').html('<pre>' + str + '</pre>');
+    });
+  })
+};
 
 
 gapi.client.setApiKey("AIzaSyCsxyRbIpVG8tvQDYbpY97ZL762_F45QWc");
-gapi.client.load("youtube", "v3", searchFunction());
-}
+gapi.client.load("youtube", "v3", init());
